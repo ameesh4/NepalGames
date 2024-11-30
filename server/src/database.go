3927d01985bcecc0a18770e2ce1632dff516547c
Amish Sharma <amishsharma.6936@gmail.com>
2024-11-30 15:56:39 +0545

    updated

---
+++ server/src/database.go
@@ -0,0 +1,26 @@
+package src
+
+import (
+	"context"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func DbConnect() (*pgxpool.Pool, error) {
+	err := godotenv.Load()
+
+	if err != nil {
+		return nil, err
+	}
+
+	conn := os.Getenv("DB_URL")
+
+	pool, err := pgxpool.New(context.Background(), conn)
+	if err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}

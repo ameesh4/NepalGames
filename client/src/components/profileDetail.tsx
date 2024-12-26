35bebb6b97723c0f04e73b4ab7d7545191a0c97b
Amish Sharma <amishsharma.6936@gmail.com>
2024-12-26 20:17:28 +0545

    category backend + editable frontend

--- client/src/components/profileDetail.tsx
+++ client/src/components/profileDetail.tsx
@@ -1,12 +1,49 @@
+import axios from "axios";
 import { useProfile } from "./ProfileContext";
 import { Button } from "./ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import { toast } from "@/hooks/use-toast";
 
+type UserUpdate = {
+    name: string
+    age: number
+    email: string
+}
 
 export default function ProfileDetail(){
     const { profile } = useProfile();
+
+    const updateProfile = () => {
+        const user: UserUpdate = {
+            name: (document.getElementById("name") as HTMLInputElement).value,
+            age: parseInt((document.getElementById("age") as HTMLInputElement).value),
+            email: profile?.email || ""
+        }
+
+        const header = {
+            headers:{
+                Authorization: localStorage.getItem("token")
+            }
+        }
+
+        axios.post("http://localhost:8000/api/edituser", user, header).then((res)=>{
+            if (res.status === 200) {
+                toast({
+                    title: "Profile Updated",
+                    description: "Your profile has been updated successfully",
+                    variant: "success",
+                })
+            }
+        }).catch((err)=>{
+            toast({
+                title: "Error",
+                description: err.response.data.message,
+                variant: "destructive"
+            })
+        })
+    }
 
     return (
         <Card>
@@ -20,13 +57,13 @@ export default function ProfileDetail(){
                 </div>
                 <div className="space-y-2">
                 <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" defaultValue={profile?.email} />
+                <Input className="cursor-not-allowed" id="email" type="email" defaultValue={profile?.email} readOnly/>
                 </div>
                 <div className="space-y-2">
                 <Label htmlFor="age">Age</Label>
                 <Input id="age" type="tel" defaultValue={profile?.age} />
                 </div>
-                <Button className="w-full">Update Profile</Button>
+                <Button className="w-full" onClick={()=>updateProfile()}>Update Profile</Button>
             </CardContent>
         </Card>
     )
